24f319cf0482ee3d5e0fcf9acba96b0c6cd47369
vedant-63git config --global user.emailvedantvaje55@gmail.com <you@example.com>
2026-06-16 23:32:41 +0530

    first commit

---
+++ arrays.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+#Creatinga ID array
+x = np.array([1,2,3])
+
+#Creating a 2D array
+y =np.array([[1,2],[3,4]])
+
+#Creating a 3D array
+z = np.array([[[1,2],[3,4]],[[5,6],[7,8]]])
+
+print(x)
+print(y)
+print(z)
